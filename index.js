17318242d1d33b9ee5ebd9a1a14336c206320f15
SanaNasar <sanasehereen.cse@gmail.com>
2014-10-26 13:51:24 -0700

    Implementing each using underscore

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 var _ = require("underscore"); // it goes and looks inside the node_modules folder
 
 var friends = ["John", "Jane"];
+
+var footastic = require("/lib/foo.js");
+
+_.each.friends = function(friend) {
+  console.log(friend);
+};
